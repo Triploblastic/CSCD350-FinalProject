98412109dfd41569a22b8f9aaae3316597b24eff
mcandertoad <mccall.andrew@gmail.com>
2014-05-27 13:28:15 -0700

    before presentation

---
+++ KeyboardMovement/src/MazeRoomLogic/ExitRoomEnterBehavior.java
@@ -0,0 +1,12 @@
+package MazeRoomLogic;
+
+public class ExitRoomEnterBehavior implements MazeRoomEnterBehavior {
+
+	//assume all exits are enterable
+	@Override
+	public boolean enter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
---
+++ KeyboardMovement/src/MazeRoomLogic/QuestionRoomEnterBehavior.java
@@ -0,0 +1,12 @@
+package MazeRoomLogic;
+
+public class QuestionRoomEnterBehavior implements MazeRoomEnterBehavior {
+
+	//for now we are assuming that all questions are answered correctly
+	@Override
+	public boolean enter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
